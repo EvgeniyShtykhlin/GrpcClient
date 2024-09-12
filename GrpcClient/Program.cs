e418586417d80156ab95013f70f625cc6d172989
Evgeniy Shtykhlin <evgeniyshtykhlin@gmail.com>
2024-09-12 14:47:41 -0700

    Add grpc client.

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Grpc.Net.Client;
+using GrpcService;
 
 namespace GrpcClient
 {
@@ -6,11 +7,17 @@ namespace GrpcClient
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter your name:");
+            var nameForGreeting = Console.ReadLine();
 
             using var chanel = GrpcChannel.ForAddress("http://localhost:5109");
-            var client= new Greeter.GreeterClient(chanel);
-            var reply = await client.SayHelloAsync(new HelloRequest { Name="Privet"});
+            var client = new Greeter.GreeterClient(chanel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = nameForGreeting });
+
+
+            Console.WriteLine("---Day Time---");
+            var reply2 = await client.DayTimeAsync(new DayTimeRequest());
+            Console.WriteLine($"Reply from daytime(): {reply2.Message}");
         }
     }
 }
