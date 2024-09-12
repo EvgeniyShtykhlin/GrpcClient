97436dac37ccc9538c3b33afb2eea2ef972d3518
Yevgeniy <Yevgeniy@DESKTOP-LLIOBPU>
2024-09-12 14:19:23 -0700

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Net.Client;
+
+namespace GrpcClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            using var chanel = GrpcChannel.ForAddress("http://localhost:5109");
+            var client= new Greeter.GreeterClient(chanel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name="Privet"});
+        }
+    }
+}
